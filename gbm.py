a494fcd92fe7bb366f33fc43cc1ee360b5071d52
wanjun3711 <143700610+wanjun3711@users.noreply.github.com>
2024-01-20 15:58:50 +0800

    Update gbm.py

--- gbm.py
+++ gbm.py
@@ -1,71 +1,74 @@
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
-from imblearn.over_sampling import RandomOverSampler
 import streamlit as st
 import joblib
 
 # 加载模型
-model_path = 'gbm_model.model'
-gbm_model = GradientBoostingClassifier()
+model_path = 'gbm_model_bone_metastasis.model'
 gbm_model = joblib.load(model_path)
 
 # 特征映射
-class_mapping = {0: "No liver metastasis", 1: "Esophagus cancer liver metastasis"}
-age_mapper = {"<70": 3, "70-80": 2, ">=80": 1}
-primary_site_mapper = {"Upper third of esophagus": 4,"Middle third of esophagus": 2,
-    "Lower third of esophagus": 3, "Overlapping lesion of esophagus": 1}
-
+feature_order = [
+    'Age', 'Sex', 'Histologic', 'Grade', 'T stage', 'N stage',
+    'Brain metastasis', 'Liver metastasis', 'Lung metastasis', 'Bone metastasis'
+]
+class_mapping = {0: "No Bone metastasis", 1: "Esophagus cancer Bone metastasis"}
+sex_mapper = {"Female": 2, "Male": 1}
 histologic_mapper = {"Adenocarcinoma": 2, "Squamous–cell carcinoma": 1}
-tumor_grade_mapper = {"Grade I": 3, "Grade II": 2, "Grade III": 1}
-t_stage_mapper = {"T1": 4, "T2": 2, "T3": 3, "T4": 1}
-n_stage_mapper = {"N0": 4, "N1": 2, "N2": 3, "N3": 1}
-surgery_mapper = {"NO": 2, "Yes": 1}
-radiation_mapper = {"NO": 2, "Yes": 1}
-chemotherapy_mapper = {"NO": 2, "Yes": 1}
-bone_metastasis_mapper = {"NO": 2, "Yes": 1}
-lung_metastasis_mapper = {"NO": 2, "Yes": 1}
+grade_mapper = {"Grade I": 3, "Grade II": 1, "Grade III": 2}
+t_stage_mapper = {"T1": 4, "T2": 1, "T3": 2, "T4": 3}
+n_stage_mapper = {"N0": 4, "N1": 1, "N2": 2, "N3": 3}
+brain_metastasis_mapper = {"NO": 0, "Yes": 1}
+liver_metastasis_mapper = {"NO": 0, "Yes": 1}
+lung_metastasis_mapper = {"NO": 0, "Yes": 1}
 
 # 预测函数
-def predict_liver_metastasis(age, primary_site, histologic, tumor_grade,
-                             t_stage, n_stage, surgery, radiation,
-                             chemotherapy, bone_metastasis, lung_metastasis):
+def predict_bone_metastasis(age, sex, histologic, grade,
+                            t_stage, n_stage, brain_metastasis, liver_metastasis, lung_metastasis, bone_metastasis):
+    sex = sex_mapper[sex]
+    histologic = histologic_mapper[histologic]
+    grade = grade_mapper[grade]
+    t_stage = t_stage_mapper[t_stage]
+    n_stage = n_stage_mapper[n_stage]
+    brain_metastasis = brain_metastasis_mapper[brain_metastasis]
+    liver_metastasis = liver_metastasis_mapper[liver_metastasis]
+    lung_metastasis = lung_metastasis_mapper[lung_metastasis]
+
     input_data = pd.DataFrame({
-        'Age': [age_mapper[age]],
-        'Primary Site': [primary_site_mapper[primary_site]],
-        'Histologic': [histologic_mapper[histologic]],
-        'Tumor grade': [tumor_grade_mapper[tumor_grade]],
-        'T stage': [t_stage_mapper[t_stage]],
-        'N stage': [n_stage_mapper[n_stage]],
-        'Surgery': [surgery_mapper[surgery]],
-        'Radiation': [radiation_mapper[radiation]],
-        'Chemotherapy': [chemotherapy_mapper[chemotherapy]],
-        'Bone metastasis': [bone_metastasis_mapper[bone_metastasis]],
-        'Lung metastasis': [lung_metastasis_mapper[lung_metastasis]]
-    })
+        'Age': [age],
+        'Sex': [sex],
+        'Histologic': [histologic],
+        'Grade': [grade],
+        'T stage': [t_stage],
+        'N stage': [n_stage],
+        'Brain metastasis': [brain_metastasis],
+        'Liver metastasis': [liver_metastasis],
+        'Lung metastasis': [lung_metastasis],
+        'Bone metastasis': [bone_metastasis],
+    }, columns=feature_order)
+
     prediction = gbm_model.predict(input_data)[0]
     probability = gbm_model.predict_proba(input_data)[0][1]  # 获取属于类别1的概率
     class_label = class_mapping[prediction]
     return class_label, probability
+
 # 创建Web应用程序
-st.title("GBM Model Predicting Liver Metastasis of Esophageal Cancer")
+st.title("GBM Model Predicting Bone Metastasis of Esophageal Cancer")
 st.sidebar.write("Variables")
 
-age = st.sidebar.selectbox("Age", options=list(age_mapper.keys()))
-primary_site = st.sidebar.selectbox("Primary site", options=list(primary_site_mapper.keys()))
-histologic = st.sidebar.selectbox("Histologic", options=list(histologic_mapper.keys()))
-tumor_grade = st.sidebar.selectbox("Tumor grade", options=list(tumor_grade_mapper.keys()))
-t_stage = st.sidebar.selectbox("T stage", options=list(t_stage_mapper.keys()))
-n_stage = st.sidebar.selectbox("N stage", options=list(n_stage_mapper.keys()))
-surgery = st.sidebar.selectbox("Surgery", options=list(surgery_mapper.keys()))
-radiation = st.sidebar.selectbox("Radiation", options=list(radiation_mapper.keys()))
-chemotherapy = st.sidebar.selectbox("Chemotherapy", options=list(chemotherapy_mapper.keys()))
-bone_metastasis = st.sidebar.selectbox("Bone metastasis", options=list(bone_metastasis_mapper.keys()))
-lung_metastasis = st.sidebar.selectbox("Lung metastasis", options=list(lung_metastasis_mapper.keys()))
+age = st.sidebar.number_input("Age", min_value=1, max_value=100, value=50)
+sex = st.sidebar.radio("Sex", options=["Male", "Female"])
+histologic = st.sidebar.selectbox("Histologic", options=["Adenocarcinoma", "Squamous–cell carcinoma"])
+grade = st.sidebar.selectbox("Grade", options=["Grade I", "Grade II", "Grade III"])
+t_stage = st.sidebar.selectbox("T Stage", options=["T1", "T2", "T3", "T4"])
+n_stage = st.sidebar.selectbox("N Stage", options=["N0", "N1", "N2", "N3"])
+brain_metastasis = st.sidebar.radio("Brain Metastasis", options=["No", "Yes"])
+liver_metastasis = st.sidebar.radio("Liver Metastasis", options=["No", "Yes"])
+lung_metastasis = st.sidebar.radio("Lung Metastasis", options=["No", "Yes"])
 
 if st.button("Predict"):
-    prediction, probability = predict_liver_metastasis(age, primary_site, histologic, tumor_grade,
-                                          t_stage, n_stage, surgery, radiation,
-                                          chemotherapy, bone_metastasis, lung_metastasis)
+    prediction, probability = predict_bone_metastasis(age, sex, histologic, grade,
+                                                 t_stage, n_stage, brain_metastasis, liver_metastasis, lung_metastasis)
 
-    st.write("Probability of developing liver metastasis：", prediction)  # 结果显示在右侧的列中
-    st.write("Probability of developing liver metastasis：", probability)  # 结果显示在右侧的列中
+    st.write("Class Label: ", prediction)  # 结果显示在右侧的列中
+    st.write("Probability of developing bone metastasis: ", probability)  # 结果显示在右侧的列中
