d6c9c666cb43fefe950faf92c995fcc6c869212b
wanjun3711 <143700610+wanjun3711@users.noreply.github.com>
2024-05-29 15:54:31 +0800

    Update gbm.py

--- gbm.py
+++ gbm.py
@@ -73,5 +73,5 @@ if st.button("Predict"):
                                                        t_stage, n_stage, surgery, chemotherapy, 
                                                        brain_metastasis, liver_metastasis, lung_metastasis)
 
-    st.write("Probability of developing Bone metastasis：", prediction)  # 结果显示在右侧的列中
-    st.write("Probability of developing Bone metastasis：", probability)  # 结果显示在右侧的列中
+    st.write("Probability of developing Bone metastasis:", prediction)
+    st.write("Probability of developing Bone metastasis:", probability)
